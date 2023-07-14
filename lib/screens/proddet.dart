83b95eb01c7cb8db88766e8b9757e3ce9dd7410c
Winsher007 <winsher200014@gmail.com>
2023-07-14 12:47:13 +0530

    winston committed

--- lib/screens/proddet.dart
+++ lib/screens/proddet.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/rating.dart';
 
 import 'contact.dart';
 
-class Proddet extends StatelessWidget {
+class Proddet extends StatefulWidget {
   const Proddet({Key? key}) : super(key: key);
+
+  @override
+  State<Proddet> createState() => _ProddetState();
+}
+
+class _ProddetState extends State<Proddet> {
+  int _rating = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -12,226 +20,244 @@ class Proddet extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
         leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(
-              Icons.arrow_back_ios_new_rounded,
-              color: Colors.black,
-            )),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: Colors.black,
+          ),
+        ),
       ),
-      body: Container(
-        child: Stack(
-          children: [
-            Positioned(
-              left: 20,
-              top: 10,
-              child: Text(
-                'Maruti Suzuki',
-                style: TextStyle(
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w500,
-                  fontSize: 28,
-                  letterSpacing: 0.02,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            Positioned(
-              left: 20,
-              top: 50,
-              child: Text(
-                'Ignis',
-                style: TextStyle(
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w600,
-                  fontSize: 20,
-                  letterSpacing: 0.02,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            Positioned(
-              left: 20,
-              top: 100,
-              child: Container(
-                width: 320,
-                height: 200,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  image: DecorationImage(
-                    image: NetworkImage(
-                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvORd7vfF74tltspL7oVQ4k6PWco6Ebmy9Bg&usqp=CAU'),
-                    fit: BoxFit.contain,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Container(
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 20,
+                  top: 10,
+                  child: Text(
+                    'Maruti Suzuki',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w500,
+                      fontSize: 28,
+                      letterSpacing: 0.02,
+                      color: Colors.black,
+                    ),
                   ),
                 ),
-              ),
-            ),
-            Positioned(
-              left: 20,
-              top: 340,
-              child: Text(
-                'Car Features',
-                style: TextStyle(
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w600,
-                  fontSize: 18,
-                  letterSpacing: 0.02,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            Positioned(
-              left: 44,
-              top: 393,
-              child: Text(
-                '5 Passengers',
-                style: TextStyle(
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w500,
-                  fontSize: 12,
-                  letterSpacing: 0.02,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            Positioned(
-              left: 44,
-              top: 427,
-              child: Text(
-                'Bluetooth',
-                style: TextStyle(
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w500,
-                  fontSize: 12,
-                  letterSpacing: 0.02,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            Positioned(
-              left: 217,
-              top: 393,
-              child: Text(
-                '4 Doors',
-                style: TextStyle(
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w500,
-                  fontSize: 12,
-                  letterSpacing: 0.02,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            Positioned(
-              left: 217,
-              top: 425,
-              child: Text(
-                'GPS',
-                style: TextStyle(
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w500,
-                  fontSize: 12,
-                  letterSpacing: 0.02,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            Positioned(
-              left: 44,
-              top: 459,
-              child: Text(
-                'Manual',
-                style: TextStyle(
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w500,
-                  fontSize: 12,
-                  letterSpacing: 0.02,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            Positioned(
-              left: 0,
-              top: 525,
-              child: Container(
-                width: 360,
-                height: 100,
-                color: Color(0xFF16161A),
-              ),
-            ),
-            Positioned(
-              left: 20,
-              top: 550,
-              child: Container(
-                width: 99,
-                height: 47,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Price',
-                      style: TextStyle(
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w600,
-                        fontSize: 14,
-                        letterSpacing: 0.02,
-                        color: Color(0xFF94A1B2),
-                      ),
+                Positioned(
+                  left: 20,
+                  top: 50,
+                  child: Text(
+                    'Ignis',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20,
+                      letterSpacing: 0.02,
+                      color: Colors.black,
                     ),
-                    Text(
-                      '₹1800/Day',
-                      style: TextStyle(
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w700,
-                        fontSize: 18,
-                        letterSpacing: 0.02,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Positioned(
-              left: 200,
-              top: 550,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => Contact(
-                              phoneNumber: '8097147818',
-                            )),
-                  ); // Handle button click event
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xFF005851),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                child: Container(
-                  width: 117,
-                  height: 50,
-                  child: Center(
-                    child: Text(
-                      'Call Now',
-                      style: TextStyle(
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w600,
-                        fontSize: 14,
-                        letterSpacing: 0.02,
-                        color: Colors.white,
+                Positioned(
+                  left: 20,
+                  top: 100,
+                  child: Container(
+                    width: 320,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvORd7vfF74tltspL7oVQ4k6PWco6Ebmy9Bg&usqp=CAU'),
+                        fit: BoxFit.contain,
                       ),
                     ),
                   ),
                 ),
-              ),
+                Positioned(
+                  left: 20,
+                  top: 340,
+                  child: Text(
+                    'Car Features',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18,
+                      letterSpacing: 0.02,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 44,
+                  top: 393,
+                  child: Text(
+                    '5 Passengers',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w500,
+                      fontSize: 12,
+                      letterSpacing: 0.02,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 44,
+                  top: 427,
+                  child: Text(
+                    'Bluetooth',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w500,
+                      fontSize: 12,
+                      letterSpacing: 0.02,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 217,
+                  top: 393,
+                  child: Text(
+                    '4 Doors',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w500,
+                      fontSize: 12,
+                      letterSpacing: 0.02,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 217,
+                  top: 425,
+                  child: Text(
+                    'GPS',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w500,
+                      fontSize: 12,
+                      letterSpacing: 0.02,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 44,
+                  top: 459,
+                  child: Text(
+                    'Manual',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w500,
+                      fontSize: 12,
+                      letterSpacing: 0.02,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  top: 525,
+                  child: Container(
+                    width: 360,
+                    height: 100,
+                    color: Color(0xFF16161A),
+                  ),
+                ),
+                Positioned(
+                  left: 20,
+                  top: 550,
+                  child: Container(
+                    width: 99,
+                    height: 47,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Price',
+                          style: TextStyle(
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14,
+                            letterSpacing: 0.02,
+                            color: Color(0xFF94A1B2),
+                          ),
+                        ),
+                        Text(
+                          '₹1800/Day',
+                          style: TextStyle(
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w700,
+                            fontSize: 18,
+                            letterSpacing: 0.02,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 250,
+                  top: 550,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Contact(
+                            phoneNumber: '8097147818',
+                          ),
+                        ),
+                      ); // Handle button click event
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF005851),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Container(
+                      width: 50,
+                      height: 20,
+                      child: Center(
+                        child: Text(
+                          'Call Now',
+                          style: TextStyle(
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w600,
+                            fontSize: 12,
+                            letterSpacing: 0.02,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 150,
+                  top: 550,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      RatingButton(), // Add RatingButton widget
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
---
+++ lib/screens/rating.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/styles.dart';
+
+class RatingButton extends StatefulWidget {
+  @override
+  _RatingButtonState createState() => _RatingButtonState();
+}
+
+class _RatingButtonState extends State<RatingButton> {
+  int _rating = 0;
+
+  void _showRatingDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Rate the Product'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Container(
+                width: 400,
+                child: Row(
+                  children: <Widget>[
+                    for (int i = 1; i <= 5; i++)
+                      IconButton(
+                        icon: Icon(
+                          i <= _rating ? Icons.star : Icons.star_border,
+                          size: 30,
+                          // Decrease the size of the stars
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _rating = i;
+                          });
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: _showRatingDialog,
+      child: Text('Rate'),
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        primary: primaryColor, // Set the button color to black
+      ),
+    );
+  }
+}
